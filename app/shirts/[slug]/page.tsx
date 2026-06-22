32938eda715e8ec0c8c2e3726cfa722be3ade039
Waqas12344 <wm635950@gmail.com>
2026-06-22 22:31:04 +0500

    feat: add ScrollIndicator component for enhanced user experience during scrolling

--- app/shirts/[slug]/page.tsx
+++ app/shirts/[slug]/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 import Scene from '@/components/Scene'
+import ScrollIndicator from '@/components/ScrollIndicator'
 import { ShirtType } from '@/lib/textures' 
-import { notFound, useParams } from 'next/navigation'
-import React from 'react' 
+import { notFound, useParams } from 'next/navigation' 
 
 const page = () => {
   const params = useParams();
@@ -10,6 +10,7 @@ const page = () => {
   if(!shirtType) return notFound;
   return <>
   <Scene shirtType={shirtType} />
+  <ScrollIndicator shirtType={shirtType} />
   </>
 }
 
